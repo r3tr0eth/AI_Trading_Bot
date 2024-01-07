a92e4030da6b6232c9c458dc1e027010918a9e72
Gaizkaja <jimenezgaizka13@protonmail.com>
2024-01-07 13:18:45 +0000

    FIX class

--- script.py
+++ script.py
@@ -1,28 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from textblob import TextBlob
 
 # Definición de las URLs de los sitios de noticias
 urls = [
-    "https://es.cointelegraph.com/",
-    "https://www.coindesk.com/es/",
-    "https://www.bloomberg.com/crypto"
+    "https://www.coindesk.com/es/"
 ]
-
-noticias_bitcoin = []
-
-for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Aquí debes ajustar las búsquedas según la estructura de cada sitio
-    # Buscando elementos que contengan noticias. Este es un ejemplo genérico.
-    for elemento in soup.find_all("h2"):  # Puede ser necesario ajustar el elemento (h1, h2, p, etc.)
-        if "bitcoin" in elemento.get_text().lower():
-            noticias_bitcoin.append(elemento.get_text().strip())
-
-    # Limita la cantidad de noticias si es necesario
-    if len(noticias_bitcoin) >= 20:
-        break
 
 enlaces_noticias = []
 
@@ -33,10 +16,31 @@ for url in urls:
     # Encuentra y guarda los enlaces a las noticias. Ajusta este código según la estructura del sitio.
     for enlace in soup.find_all("a"):  # Ajusta el selector según sea necesario
         titulo = enlace.get_text().strip()
-        if "bitcoin" in titulo.lower():
+        if any(palabra in titulo.lower() for palabra in ["crypto", "ethereum", "bitcoin", "solana"]):
             enlaces_noticias.append(enlace['href'])  # Asegúrate de que 'href' es el atributo correcto
 
-    if len(enlaces_noticias) >= 20:
+    if len(enlaces_noticias) >= 30:
         break
 
-print(enlaces_noticias)
+# URL base para cada sitio web de noticias
+base_url_coindesk = "https://www.coindesk.com"
+
+# Concatenar la URL base con las rutas relativas
+urls_completas_coindesk = [base_url_coindesk + ruta for ruta in enlaces_noticias if ruta.startswith("/markets") or ruta.startswith("/tech") or ruta.startswith("/policy")]
+
+# Diccionario para almacenar los resultados
+
+cuerpos_noticias = []
+
+for url in urls_completas_coindesk:
+    response = requests.get(url)
+    # Asegúrate de manejar excepciones y posibles errores de respuesta aquí
+    
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Aquí necesitas encontrar el selector correcto para el cuerpo de las noticias
+    # Este es un selector genérico, tendrás que ajustarlo según el sitio web
+    cuerpo_noticia = soup.find('div', class_='containerstyles__StyledContainer-sc-292snj-0 KqMZq article-bodystyles__StyledWrapper-sc-5qmdhl-0 fEKnok').get_text()
+    cuerpos_noticias.append(cuerpo_noticia)
+
+print(cuerpos_noticias)
